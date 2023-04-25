63a519483220bba878ff8fd16667573e5d5d64f8
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-04-25 12:49:06 +0530

    reload movie before button click

--- src/App.js
+++ src/App.js
@@ -2,40 +2,59 @@ import React from 'react';
 import { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
-
+import { useEffect,useCallback } from 'react';
 function App() {
-  const [value,setValue]=useState([]);
-  const [isLoading,setIsLoading]=useState(false);
-  function fetchMoviesHandler(){
+  const [value, setValue] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // const [reload,setReload]=useState(false)
+ const fetchMoviesHandler=useCallback(async ()=> {
     setIsLoading(true);
-    fetch('https://swapi.dev/api/films/').then(resolve=>{
-        return resolve.json();
-    }).then(data=>{
-      console.log(data);
-      const newVal=  data.results.map(val=>{
-            return {
-              id:val.episode_id,
-              title:val.title,
-              openingText:val.opening_crawl,
-              releaseDate:val.release_date
-            }
-        })
-        setValue(newVal);
-        setIsLoading(false)
-    })
-
+    try {
+      const resolve = await fetch('https://swapi.dev/api/films/')
+      if (!resolve.ok) {
+        throw new Error('something went wrong ...retrying')
+      }
+      const data = await resolve.json();
+      const newVal = data.results.map(val => {
+        return {
+          id: val.episode_id,
+          title: val.title,
+          openingText: val.opening_crawl,
+          releaseDate: val.release_date
+        }
+      })
+      setValue(newVal)
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  },[])
+  //  fetchMoviesHandler;
+  useEffect(()=>{
+     fetchMoviesHandler()
+ },[fetchMoviesHandler])
+// if(error!==null){
+//    var t= setInterval(fetchMoviesHandler, 5000);
+// }
+  function btnHandler() {
+    setError(null);
+    // clearInterval(t);  
+    // setIsLoading(true)
   }
-  
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-       {isLoading?<h1>Loader</h1>: <MoviesList movies={value} />}
-      </section>
-    </React.Fragment>
-  );
-}
+    return (
+      <React.Fragment>
+        <section>
+          <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        </section>
+        <section>
+          {! isLoading && value.length>0 && <MoviesList movies={value} />}
+          {isLoading && <p>Loading.....</p>}
+          {!isLoading && error && <>{error} <button onClick={btnHandler}>Stop retrying</button></>}
+        </section>
+      </React.Fragment>
+    );
+  }
+ 
 
-export default App;
+  export default App;
